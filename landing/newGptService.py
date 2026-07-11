2028c362cdf0e57fa97eec5e43aabb589d13155b
Glurk <yamokasy2@gmail.com>
2026-07-11 22:34:04 +0300

    upd

--- landing/newGptService.py
+++ landing/newGptService.py
@@ -2,22 +2,22 @@ import openai
 from django.conf import settings
 
 
-
-openai.api_key = settings.NEW_CHAT_GPT_KEY
-
-
 class newGptService:
 
+    @staticmethod
     def sendPrompt(prompt, maxT):
-        response = openai.Completion.create(
-            model='gpt-3.5-turbo-instruct',
-            prompt= prompt, 
+        client = openai.OpenAI(api_key=settings.NEW_CHAT_GPT_KEY)
+
+        response = client.chat.completions.create(
+            model='gpt-4o-mini',
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
             max_tokens=maxT
-        )    
-        data = response.choices[0].text
+        )
+
+        data = response.choices[0].message.content
+
         data_without_linebreaks = data.replace('\n', '').replace('\r', '').replace('"', '')
 
         return data_without_linebreaks
-
-
-# newGptService = newGptService()
